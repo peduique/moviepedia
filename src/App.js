9806d9050a4c659fff5d2d182c19df7983f40e3f
Pedro Taglialatela <pedrotaglialatela@Pedros-MacBook-Pro.local>
2020-12-22 18:55:51 -0300

    initiate project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { lazy, Suspense } from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-function App() {
+import GlobalStyle from './styles/global';
+
+const HomePage = lazy(() => import('~/pages/Home'));
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <GlobalStyle />
+
+      <Suspense fallback={<h1>Loading...</h1>}>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+        </Switch>
+      </Suspense>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
